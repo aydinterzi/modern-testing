4a61a291ea2a1c26169cdbec9183bb2a0c40e8d4
Aydin Terzi <aydinterzi7@gmail.com>
2024-04-23 19:17:50 +0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+it("should render App component without crashing", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("Modern Testing")).toBeInTheDocument();
 });
---
+++ src/components/button/button.test.js
@@ -0,0 +1,16 @@
+describe("Button", () => {
+  describe("when variant primary", () => {
+    it("should render the primary button", () => {
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+  describe("when variant secondary", () => {
+    it("should render the secondary button", () => {
+      // Arrange
+      // Act
+      // Assert
+    });
+  });
+});

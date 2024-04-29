214512b210d9ec86b2762c7bf833a589f195238d
Aydin Terzi <aydinterzi7@gmail.com>
2024-04-29 13:02:42 +0300

    multiple element query

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+
+function TestComponent({ products }) {
+  return (
+    <ul>
+      {products.map((product, index) => (
+        <li key={index}>{product}</li>
+      ))}
+    </ul>
+  );
+}
 
 it("should render App component without crashing", () => {
-  render(<App />);
-  expect(screen.getByText("Modern Testing")).toBeInTheDocument();
+  render(<TestComponent products={["product 1", "product 2", "product 3"]} />);
+  const elements = screen.getAllByRole("listitem");
+  expect(elements).toHaveLength(3);
 });

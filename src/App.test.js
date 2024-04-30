3bb7a84118049092acc2f08c928d45c3517cef50
Aydin Terzi <aydinterzi7@gmail.com>
2024-04-30 13:18:19 +0300

    Mouse/Pointer Events

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,28 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useState } from "react";
 
-function TestComponent({ suffix }) {
+function TestComponent() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      {suffix && suffix}
-      {!suffix && "suffix yok"}
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
 
-it("should render App component without crashing", () => {
-  render(<TestComponent suffix="Test" />);
-  const element = screen.getByText(/Test/i);
-  const emptyElement = screen.queryByText("suffix yok");
+it("should render the element correctly", async () => {
+  const user = userEvent.setup();
+  render(<TestComponent />);
 
-  expect(emptyElement).not.toBeInTheDocument();
-  expect(element).toBeInTheDocument();
+  await user.pointer({
+    keys: "[MouseLeft]",
+    target: screen.getByText("Increment"),
+  });
+
+  const h1Element = await screen.findByRole("heading");
+
+  expect(h1Element).toHaveTextContent("1");
 });
